32180027b9c63fcea2420438f2b464bd1b6e083d
FreedomEngineering <freedomengineering42@gmail.com>
2018-11-11 10:44:36 -0800

    add to / remove from cart button

--- src/js/Root.js
+++ src/js/Root.js
@@ -7,26 +7,65 @@ import { Modal } from "./components/Modal";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      modalProps: {}
-    };
-
     this.onModalClick = this.onModalClick.bind(this);
+    this.handleCartButtonClick = this.handleCartButtonClick.bind(this);
+    this.getCart = this.getCart.bind(this);
+
+    const cart = this.getCart();
+
+    this.state = {
+      modalProps: {},
+      cart
+    };
   }
+
+  getCart() {
+    const serializedCart = window.localStorage.getItem("cart");
+    let cart;
+    if (!serializedCart) {
+      cart = [];
+    } else {
+      cart = JSON.parse(serializedCart);
+    }
+    return cart;
+  }
+
+  handleCartButtonClick(id) {
+    const { cart } = this.state;
+
+    const existingItemIndex = cart.indexOf(id);
+    const isItemInCart = existingItemIndex > -1;
+    let newCart;
+    if (isItemInCart) {
+      newCart = cart
+        .slice(0, existingItemIndex)
+        .concat(cart.slice(existingItemIndex + 1));
+    } else {
+      newCart = cart.concat(id);
+    }
+
+    this.setState({ cart: newCart }, () =>
+      window.localStorage.setItem("cart", JSON.stringify(newCart))
+    );
+  }
+
   onModalClick(modalProps) {
-    console.log("clicked");
     this.setState({
       modalProps
     });
   }
 
   render() {
-    const { modalProps } = this.state;
+    const { modalProps, cart } = this.state;
     return (
       <div id="homePage">
         <Jumbotron />
         <ProductsContainer onModalClick={this.onModalClick} />
-        <Modal {...modalProps} />
+        <Modal
+          {...modalProps}
+          cart={cart}
+          handleCartButtonClick={this.handleCartButtonClick}
+        />
       </div>
     );
   }
--- src/js/components/Modal.js
+++ src/js/components/Modal.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 
 import { formatPrice } from "../../../lib/utils";
 
@@ -14,77 +14,91 @@ export const ModalButton = ({ onClick }) => (
   </button>
 );
 
-export const Modal = ({
-  name,
-  images,
-  shortDescription,
-  longDescription,
-  price
-}) => (
-  <div
-    id="productModal"
-    className="modal fade"
-    tabIndex="-1"
-    role="dialog"
-    aria-labelledby="myLargeModalLabel"
-    aria-hidden="true"
-  >
-    <div className="modal-dialog modal-lg modal-dialog-centered">
-      <div className="modal-content">
-        {!images ? null : (
-          <Fragment>
-            <div className="modal-header">
-              <button
-                type="button"
-                className="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div id="productPage" className="container">
-              <div className="row">
-                <div className="col-xs-12 col-sm-6 mainImage">
-                  <img
-                    className="img-fluid shadow p-3 bg-white rounded"
-                    src={images[0].src}
-                    alt={images[0].alt}
-                  />
+export class Modal extends Component {
+  render() {
+    const {
+      cart,
+      handleCartButtonClick,
+      id,
+      name,
+      images,
+      shortDescription,
+      longDescription,
+      price
+    } = this.props;
+    const isItemInCart = cart.indexOf(id) > -1;
+
+    return (
+      <div
+        id="productModal"
+        className="modal fade"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="myLargeModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-lg modal-dialog-centered">
+          <div className="modal-content">
+            {!id ? null : (
+              <Fragment>
+                <div className="modal-header">
+                  <button
+                    type="button"
+                    className="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                  >
+                    <span aria-hidden="true">&times;</span>
+                  </button>
                 </div>
-                <div className="col-xs-12 col-sm-6">
-                  <h1>{name}</h1>
-                  <h6 className="price">{`Price: $${formatPrice(price)}`}</h6>
-                  <hr />
-                  <h4 className="shortDescription">{shortDescription}</h4>
-                  <div className="form-row flex-container">
-                    <div className="col addToCart">
-                      <button
-                        type="button"
-                        className={`btn btn-primary btn-lg`}
-                        onClick={() => {}}
-                      >
-                        Add To Cart
-                      </button>
+                <div id="productPage" className="container">
+                  <div className="row">
+                    <div className="col-xs-12 col-sm-6 mainImage">
+                      <img
+                        className="img-fluid shadow p-3 bg-white rounded"
+                        src={images[0].src}
+                        alt={images[0].alt}
+                      />
+                    </div>
+                    <div className="col-xs-12 col-sm-6">
+                      <h1>{name}</h1>
+                      <h6 className="price">{`Price: $${formatPrice(
+                        price
+                      )}`}</h6>
+                      <hr />
+                      <h4 className="shortDescription">{shortDescription}</h4>
+                      <div className="form-row flex-container">
+                        <div className="col addToCart">
+                          <button
+                            type="button"
+                            className={`btn btn-${
+                              isItemInCart ? "warning" : "primary"
+                            } btn-lg`}
+                            onClick={() => handleCartButtonClick(id)}
+                          >
+                            {isItemInCart ? "Remove From Cart" : "Add To Cart"}
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    <hr />
+                    <div className="row longDescription">
+                      <div className="col-xs-12">
+                        <h3>Product Description</h3>
+                        {longDescription.map(desc => (
+                          <p key={desc.slice(0, 10)} className="text-justify">
+                            {desc}
+                          </p>
+                        ))}
+                      </div>
                     </div>
                   </div>
                 </div>
-                <hr />
-                <div className="row longDescription">
-                  <div className="col-xs-12">
-                    <h3>Product Description</h3>
-                    {longDescription.map(desc => (
-                      <p key={desc.slice(0, 10)} className="text-justify">
-                        {desc}
-                      </p>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Fragment>
-        )}
+              </Fragment>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
